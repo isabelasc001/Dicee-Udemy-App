381209d0fb3557dce4d49d9d534baee80b64becf
isabelasc001 <isabelasc2001@rede.ulbra.br>
2024-07-17 19:59:42 -0300

    Envio app dicee

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,36 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
+//who.what = value
+//firstDiceImageView.image = .init(named: "DiceSix")
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //refereces a UI element
+    @IBOutlet weak var firstDiceImageView: UIImageView!
+    
+    @IBOutlet weak var secondDiceImageView: UIImageView!
+    
+    
+    @IBAction func tappedRollButton(_ sender: UIButton) {
+        firstDiceImageView.image = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")
+        ][Int.random(in: 0...5)]
+        
+        secondDiceImageView.image = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")
+        ] [Int.random(in: 0...5)]
+        
+        
     }
-
-
 }
 
